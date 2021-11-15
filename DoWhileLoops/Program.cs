52501405d50c6a0ce05ce8cd6fc184958f5a15e9
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-15 15:29:02 -0700

    added a while loop into my program

--- DoWhileLoops/Program.cs
+++ DoWhileLoops/Program.cs
@@ -40,6 +40,81 @@ namespace DoWhileLoops
                 }
             }
             while (!user);
+            Console.WriteLine("Please type in a month");
+            string month = Console.ReadLine();
+            bool favMonth = false;
+
+            while (!favMonth)
+            {
+                switch (month)
+                {
+                    case "January":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "March":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "April":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "February":
+                        Console.WriteLine("February is the best month");
+                        favMonth = true;
+                        break;
+                    case "May":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "June":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "July":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "August":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "September":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "October":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "November":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    case "December":
+                        Console.WriteLine(month + " is not the best month");
+                        Console.WriteLine("Please type in a month");
+                        month = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine(month + " is not recognized as a month.");
+                        Console.WriteLine("Try again.");
+                        month = Console.ReadLine();
+                        break;
+                }
+                
+            }
             Console.ReadLine();
         }
     }
