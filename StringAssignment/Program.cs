b66ee4929369b9ace1255513ddd06695a95b130b
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-15 15:53:37 -0700

    created a program using c# and visual studio that plays with some string functions

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite sport?");
+            string sport = Console.ReadLine();
+            Console.WriteLine("Which team is your favorite?");
+            string team = Console.ReadLine();
+
+            string sentence = "My name is " + name + " and my favorite sport is " + sport + ". My favorite team is the " + team + ".";
+
+            Console.WriteLine(sentence);
+            Console.WriteLine(team.ToUpper());
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is the first sentence. ");
+            sb.Append("\nThis is the second sentence. ");
+            sb.Append("\nThis is the third sentence. ");
+            sb.Append("\nThis is the fourth sentence.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
