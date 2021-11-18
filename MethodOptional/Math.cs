58438c2ec9f0d8938df2f1cfd2074f6eea6e98d3
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-17 21:34:06 -0700

    created a method that uses an optional parameter

---
+++ MethodOptional/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptional
+{
+    class Math
+    {
+        public int OptionalMath(int userNum, int optionalNum = 4)
+        {
+            int result = userNum * optionalNum;
+            return result;
+        }
+    }
+}
---
+++ MethodOptional/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please type in your first number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type in your optional number here. If you do not include a number, it will default to 4.");
+            try
+            {
+                string userNum2 = Console.ReadLine();
+                if (userNum2 != "")
+                {
+                    int optionalNum = Convert.ToInt32(userNum2);
+                    int response = math.OptionalMath(userNum, optionalNum);
+                    Console.WriteLine(userNum + " multiplied by " + optionalNum + " equals: " + response);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    int response = math.OptionalMath(userNum);
+                    Console.WriteLine(userNum + " multiplied by 4 equals: " + response);
+                    Console.ReadLine();
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type in a valid input");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            } 
+        }
+    }
+}
