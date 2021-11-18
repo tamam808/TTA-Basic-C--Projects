8d6160ecbbabafbef65e8837b42df99a37750ebe
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-17 19:23:21 -0700

    created a program that utilizes try/catch blocks to stop user errors from ending the program, instead displaying error messages to the user

---
+++ TryCatch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 10, 15, 32, 8, 77, 21 };
+            try
+            {
+                Console.WriteLine("Enter a positive number.");
+                int userNum = Convert.ToInt32(Console.ReadLine());
+                foreach (int number in numbers)
+                {
+                    double dividedBy = (double)number / (double)userNum;
+                    string newNum = number + " divided by " + userNum + " equals: " + dividedBy;
+                    Console.WriteLine(newNum);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide by zero.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("You must enter a number");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The program will now carry on.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
