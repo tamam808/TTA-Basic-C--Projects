b4411c26767a717e43437d1accd7106e0cf3997e
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-17 20:20:03 -0700

    created a program that utilizes classes and methods to do math operations on a user's input

---
+++ ClassesAndMethods/MathMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class MathMethods
+    {
+        public int Times5(int userNum)
+        {
+            int result;
+            result = userNum * 5;
+            return result;
+        }
+        public double DivideBy2(int userNum)
+        {
+            double result;
+            result = (double)userNum / 2;
+            return result;
+        }
+        public int Plus50(int userNum)
+        {
+            int result;
+            result = userNum + 50;
+            return result;
+        }
+    }
+}
---
+++ ClassesAndMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            MathMethods m = new MathMethods();
+            double divided = m.DivideBy2(userNum);
+            int multiplied = m.Times5(userNum);
+            int added = m.Plus50(userNum);
+            Console.WriteLine(userNum + " multiplied by 5 equals: " + multiplied);
+            Console.WriteLine(userNum + " divided by 2 equals: " + divided);
+            Console.WriteLine(userNum + " added by 50 equals: " + added);
+            Console.ReadLine();
+        }
+    }
+}
