7f631f23675d9bbead5d94714322f70b08fd260d
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-17 21:01:58 -0700

    created a program that uses method overloading to call methods of the same names that ask for different parameters

---
+++ MethodOverload/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Math
+    {
+        public int Times5(int userNum)
+        {
+            int result = userNum * 5;
+            return result;
+        }
+
+        public int Times5(int userNum, decimal myDecimal)
+        {
+            decimal result = userNum * 5 * myDecimal;
+            Convert.ToInt32(result);
+            return (int)result;
+        }
+
+        public int Times5(int userNum, string numAsString)
+        {
+            int stringToInt = Convert.ToInt32(numAsString);
+            int result = userNum * 5 * stringToInt;
+            return result;
+        }
+    }
+}
---
+++ MethodOverload/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Math math = new Math();
+            int method1 = math.Times5(userNum);
+            Console.WriteLine(userNum + " multiplied by 5 equals: " + method1);
+            Console.WriteLine("Now type in a decimal.");
+            decimal myDecimal = Convert.ToDecimal(Console.ReadLine());
+            int method2 = math.Times5(userNum, myDecimal);
+            Console.WriteLine(userNum + " multiplied by 5 and " + myDecimal + " equals/rounds up to: " + method2);
+            Console.WriteLine("Type in another number. This number will not be converted to integer immediately.");
+            string numAsString = Console.ReadLine();
+            int method3 = math.Times5(userNum, numAsString);
+            Console.WriteLine(userNum + " multiplied by 5 and \"" + numAsString + "\" equals: " + method3);
+            Console.ReadLine();
+        }
+    }
+}
