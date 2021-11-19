cf958d7ba997c009b10c0f08b4853c31e5ec2e8d
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-18 18:17:29 -0700

    created a program that explores void methods that don't require a result to be returned

---
+++ MethodVoid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //call the VoidMethod class and save it as math
+            VoidMethod math = new VoidMethod();
+            Console.WriteLine("Please type in your first number");
+            //ask for the user to input a number. save it as userNum1
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please type in your second number");
+            //ask for another number. save it as userNum2
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            //call Method1 and pass it the arguments that have been provided by the user
+            math.Method1(userNum1, userNum2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodVoid/VoidMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodVoid
+{
+    class VoidMethod
+    {
+        //created a void method which means it doesn't need to return a value
+        public void Method1(int userNum1, int userNum2)
+        {
+            //do a math operation to the 1st integer
+            int result = userNum1 * 100;
+            //display the 2nd integer
+            Console.WriteLine(userNum2);
+        }
+    }
+}
