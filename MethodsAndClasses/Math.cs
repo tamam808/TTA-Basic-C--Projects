efbccfd7fd1255851b04595e979d38dde1fe8ae7
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-11-22 14:45:42 -0700

    created a program used to practice using static classes and methods as well as output parameters

---
+++ MethodsAndClasses/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    //created a static class
+    static class Math
+    {
+        //create void method Divide
+        public static void Divide(int userNum)
+        {
+            int result = userNum / 2;
+            Console.WriteLine(userNum + " divided by 2 equals: " + result);
+        }
+        //overload Divide method with an out parameter
+        public static int Divide(int userNum, int userNum2, out int times5)
+        {
+            times5 = userNum2 * 5;
+            int result = userNum / 2;
+            return result;
+        }
+    }
+}
---
+++ MethodsAndClasses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            //save the user's input as var userNum and call the Math class' Divide method
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Math.Divide(userNum);
+            Console.WriteLine("Please input another number");
+            //save the user's input as var userNum2
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            //call Math class' Divide method and save the result as var answers
+            int answers = Math.Divide(userNum, userNum2, out int times5);
+            Console.WriteLine($"{userNum} divided by 2 equals: {answers}");
+            Console.WriteLine($"{userNum2} multiplied by 5 equals: {times5}");
+            Console.ReadLine();
+        }
+    }
+}
