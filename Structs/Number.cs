a9772523ff74ea9d53998f426bb42d0050f75624
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-12-01 03:20:24 -0700

    created a program that practices creating structs

---
+++ Structs/Number.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    public struct Number
+    {
+        //create a struct named Number and give it a decimal property called Amount
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Structs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of data type Number
+            Number myNum = new Number();
+            //assign it a value
+            myNum.Amount = 24.80m;
+            //print to console
+            Console.WriteLine(myNum.Amount);
+            Console.ReadLine();
+        }
+    }
+}
