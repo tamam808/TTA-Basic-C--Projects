086abcffba4a4e8541c5928385e4d42363cf574b
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-12-02 02:34:22 -0700

    created a program using C# to practice logging certain events to a txt file

---
+++ IO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number that I can log into a text file.");
+            //save the user's input as a string with a concatenated sentence
+            string userNum = "Your number is " + Console.ReadLine();
+            Console.WriteLine("Logging...");
+            //use File.WriteAllText to write userNum into log.txt. this line will create a new file called log.txt if it doesn't already exist
+            File.WriteAllText(@"C:\Users\Tama\Documents\GitHub\TTA-Basic-Csharp-Projects\IO\log.txt", userNum);
+            Console.WriteLine("This is the text that was written into the .txt file.");
+            //save the text written in log.txt under a variable named fileText and write that back to the console
+            string fileText = File.ReadAllText(@"C:\Users\Tama\Documents\GitHub\TTA-Basic-Csharp-Projects\IO\log.txt");
+            Console.WriteLine(fileText);
+            Console.ReadLine();
+        }
+    }
+}
