59dedb6fb890ac2497b2259c587e3afeecbb04de
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-12-06 03:05:49 -0700

    created a program that practices using try/catch blocks to catch exceptions thrown by the program and show the user an error message

---
+++ TryCatchAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?"); 
+            try
+            {
+                byte age = Convert.ToByte(Console.ReadLine());
+                if (age == 0) throw new OverflowException();
+                int currentYear = DateTime.Now.Year;
+                int yearBorn = currentYear - (int)age;
+                Console.WriteLine($"You are {age} years old so you were born in {yearBorn}.");
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Your input included a letter/letters. Please input numbers only.");
+                Console.ReadLine();
+                return;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Your number was negative or zero. Please input positive numbers only.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something you have entered has caused an unexpected error to occur. Please try again later");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
