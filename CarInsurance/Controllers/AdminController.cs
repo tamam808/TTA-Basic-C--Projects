a4ca61536f96fc50a4bd1e1a97b0e0cc57978c42
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-12-13 17:56:35 -0700

    created a car insurance app

---
+++ CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+        
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees;
+                var insureesVM = new List<Insuree>();
+
+                foreach (var insuree in insurees)
+                {
+                    var insureeVM = new Insuree();
+                    insureeVM.FirstName = insuree.FirstName;
+                    insureeVM.LastName = insuree.LastName;
+                    insureeVM.EmailAddress = insuree.EmailAddress;
+                    insureeVM.Quote = insuree.Quote;
+                    insureesVM.Add(insuree);
+                }
+
+                return View(insureesVM);
+            }
+            
+            
+        }
+    }
+}
