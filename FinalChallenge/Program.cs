5c4f17114845d82041bab09d20b4ef280579e471
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-12-14 19:54:05 -0700

    created a console app that uses code-first entity framework technology

---
+++ FinalChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
